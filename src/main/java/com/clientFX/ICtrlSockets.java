78c570c7872164c52a84b61b3a4e908d3761a024
ruben-bena <181500053+ruben-bena@users.noreply.github.com>
2025-10-03 17:11:00 +0200

    .

---
+++ src/main/java/com/clientFX/ICtrlSockets.java
@@ -0,0 +1,9 @@
+package com.clientFX;
+
+public interface ICtrlSockets {
+    // para colocar la ficha en el placeholder cercano
+    void setDiscOnCloserPlaceholder();
+
+    // para atualizar la estructura de datos que guarda la información de las fichas en la graella (una matriz por ejemplo)
+    void update
+}
