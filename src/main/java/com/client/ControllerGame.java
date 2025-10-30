150d00019286c9e92d3683721218927ecb6e17ac
ruben-bena <ruben.bellido.navarro@gmail.com>
2025-10-30 19:38:46 +0100

    Formatted class ControllerGame: unused code and comments deleted, identation improved

--- src/main/java/com/client/ControllerGame.java
+++ src/main/java/com/client/ControllerGame.java
@@ -35,7 +35,6 @@ public class ControllerGame implements Initializable {
 
     public static Color boardColor = new Color(0,0,0.5,1);
     private static Color redColor = new Color(0.5,0,0,1);
-    private static Color blueColor = new Color(0,0,0.5,1);
     private static Color yellowColor = new Color(0.7,0.6,0.3,1);
 
     public static double currentChip_x, currentChip_y;
@@ -43,7 +42,7 @@ public class ControllerGame implements Initializable {
 
     public static ArrayList<Integer> possibleMovesList = new ArrayList<>();
 
-    // Animación de caida
+    // Chip falling animation
     private boolean animating = false;
     private int animCol = -1, animRow = -1;
     private double animY;
@@ -54,9 +53,6 @@ public class ControllerGame implements Initializable {
     private static double animChip_x, animChip_y;
     private static boolean isAnimChip;
     private static int animChipPlayer;
-
-    // redDraggableChip = new DraggableChip(draggableChips_red_x, draggableChips_red_y, redColor, this.player1);
-    // yellowDraggableChip = new DraggableChip(draggableChips_yellow_x, draggableChips_yellow_y, yellowColor, this.player2);
 
     // Winner
     private static String winner = "none";
@@ -71,7 +67,7 @@ public class ControllerGame implements Initializable {
     private GameArtist gameArtist;
 
     public static void updateWinnerLine(String line) {
-        // formato "x_start x_end y_start y_end"
+        // format "x_start x_end y_start y_end"
         if(line.equals("none")) return;
 
         String[] parts = line.split(" ");
@@ -79,7 +75,6 @@ public class ControllerGame implements Initializable {
         winner_end_x = Double.parseDouble(parts[1]);
         winner_start_y = Double.parseDouble(parts[2]);
         winner_end_y = Double.parseDouble(parts[3]);
-
     }
 
     public static void updateWinner(String winn) {
@@ -98,10 +93,12 @@ public class ControllerGame implements Initializable {
         animChip_y = Double.parseDouble(parts[2]);
         animChipPlayer = Integer.parseInt(parts[3]);
     }
+
     public static void updateBoardPos(double x, double y) {
         board_x = x;
         board_y = y;
     }
+
     public static void updateCurrentChip(String chip) {
         if (chip.equals("none")) {
             currentChip_player = 0;
@@ -112,7 +109,6 @@ public class ControllerGame implements Initializable {
         currentChip_x = Double.parseDouble(parts[0]);
         currentChip_y = Double.parseDouble(parts[1]);
         currentChip_player = Integer.parseInt(parts[2]);
-
     }
     public static void updateDragChipsPos(double red_x, double red_y, double yellow_x, double yellow_y) {
         red_chip_dragg_x = red_x;
@@ -120,6 +116,7 @@ public class ControllerGame implements Initializable {
         yellow_chip_dragg_x = yellow_x;
         yellow_chip_dragg_y = yellow_y;
     }
+
     public static void setPlayerInfo(String nombre, int numero, boolean draggea) {
         name = Main.clientName;
         playerNum = numero;
@@ -190,8 +187,6 @@ public class ControllerGame implements Initializable {
             gc.setLineCap(StrokeLineCap.ROUND);
             gc.strokeLine(winner_start_x, winner_start_y, winner_end_x, winner_end_y);
         }
-
-        
     }
 
     @Override
@@ -202,28 +197,18 @@ public class ControllerGame implements Initializable {
 
         canvas.setOnMouseMoved(event -> {
             Main.sendPlayerMousePosInfo(Main.clientName, event.getSceneX(), event.getSceneY(), dragging);
-
-            //TODO Aquí hacer que mande un json con la info necesaria
         });
 
         canvas.setOnMouseDragged(event -> {
             dragging = true;
             Main.sendPlayerMousePosInfo(Main.clientName, event.getSceneX(), event.getSceneY(), dragging);
-            
-
-            //TODO Aquí hacer que mande un json con la info necesaria
         });
 
         canvas.setOnMouseReleased(event -> {
             dragging = false;
             Main.sendPlayerMousePosInfo(Main.clientName, event.getSceneX(), event.getSceneY(), dragging);
-            
-            // game.checkReleases();
-
-            //TODO Aquí hacer que mande un json con la info necesaria
-
         });
-}
+    }
 
     public interface drawable {
             void draw();
@@ -298,21 +283,15 @@ public class ControllerGame implements Initializable {
                     return false;
                 }
 
-
-                
-
-                
-
                 @Override
                 public void draw() {
                     gc.setFill(boardColor);
                     gc.fillRect(board_x, board_y, width + margin, height + space_between);
-                                        //rows
+                    
                     for(int i = 0; i < grid.length; i++) {
-                    // for(int i = 0; i < 7; i++) {
-                                        // cols
+
                         for(int j = 0; j < grid[0].length; j++) {
-                        // for(int j = 0; j < 6; j++) {
+
                             double pos_x = board_x + margin/2 + j * (CELL_SIZE + space_between);
                             double pos_y = board_y + margin/2 + i * (CELL_SIZE + space_between);
                             gc.setFill(Color.WHITE);
@@ -354,109 +333,76 @@ public class ControllerGame implements Initializable {
 
             }
         
-        class ChipArtist implements drawable {
-            private double diameter;
-            private Color color;
-            private double x,y;
+    class ChipArtist implements drawable {
+    private double diameter;
+    private Color color;
+    private double x,y;
 
-            public ChipArtist(double diameter, int player) {
-                if (player == 1) {
-                    this.color = redColor;
-                } else {
-                    this.color = yellowColor;
-                }
-
-                this.diameter = diameter;
-
-            }
-
-            public void setPosition(double x, double y) {
-                this.x = x;
-                this.y = y;
-            }
-            public void draw() {
-                gc.setFill(color);
-                gc.fillOval(this.x, this.y, diameter, diameter);
-            }
-
-            public void drawOnPlayer(double player_x, double player_y) {
-                gc.setFill(color);
-                x = player_x - diameter/2;
-                y = player_y - diameter/2;
-                draw();
-            }
+    public ChipArtist(double diameter, int player) {
+        if (player == 1) {
+            this.color = redColor;
+        } else {
+            this.color = yellowColor;
         }
 
-        class PlayerArtist implements drawable {
-            private double radius = CELL_SIZE/3;
-            private Color color1 = new Color(1,0,0,0.5);
-            private Color color2 = new Color(0,1,1,0.5);
-
-            public PlayerArtist() {
-        
-            }
-
-            @Override
-            public void draw() {
-                if (playerNum == 1) {
-                    gc.setFill(color1);
-                } else {
-                    gc.setFill(color2);
-                }
-                gc.fillOval(mouse_x - radius/2, mouse_y - radius/2, radius, radius);
-            }
-        }
-        
-        class GameArtist implements drawable {
-            @Override
-            public void draw() {
-                boardArtist.draw();
-                playerArtist.draw();
-                // player2.artist.draw();
-            }
-
-            public void drawWinnerLine() {
-                gc.setFill(yellowColor);
-                gc.setStroke(Color.BLACK);
-                gc.setLineWidth(20);
-                gc.setLineCap(StrokeLineCap.ROUND);
-                gc.strokeLine(winner_start_x, winner_start_y, winner_end_x, winner_end_y);
-            }
-
-            public void drawBoard() {
-                boardArtist.draw();
-            }
-
-            public void drawChip() {
-                chipArtist.draw();
-            }
-
-            // public void drawDraggableChips() {
-            //     // Draw red chips
-            //     redDraggableChip.artist.draw();
-            //     yellowDraggableChip.artist.draw();
-
-            // }
-
-            // public void drawChipsDragging() {
-            //     if (isPlayerDraggingChip(player1, redDraggableChip)) {
-            //         currentChip = createChip(1);
-            //         currentChip.artist.drawOnPlayer(player1);
-            //         // Drawing the player again so it appears on top of the chip
-            //         player1.artist.draw();
-            //         board.artist.drawPossibleMoves();
-            //     }
-            //     if (isPlayerDraggingChip(player2, yellowDraggableChip)) {
-            //         currentChip = createChip(2);
-            //         currentChip.artist.drawOnPlayer(player2);
-            //         // Drawing the player again so it appears on top of the chip
-            //         player2.artist.draw();
-            //         board.artist.drawPossibleMoves();
-            //     }
-            // }
-        }
+        this.diameter = diameter;
     }
 
+    public void setPosition(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
 
-    
+    public void draw() {
+        gc.setFill(color);
+        gc.fillOval(this.x, this.y, diameter, diameter);
+    }
 
+    public void drawOnPlayer(double player_x, double player_y) {
+        gc.setFill(color);
+        x = player_x - diameter/2;
+        y = player_y - diameter/2;
+        draw();
+    }
+}
+
+    class PlayerArtist implements drawable {
+        private double radius = CELL_SIZE/3;
+        private Color color1 = new Color(1,0,0,0.5);
+        private Color color2 = new Color(0,1,1,0.5);
+
+        @Override
+        public void draw() {
+            if (playerNum == 1) {
+                gc.setFill(color1);
+            } else {
+                gc.setFill(color2);
+            }
+            gc.fillOval(mouse_x - radius/2, mouse_y - radius/2, radius, radius);
+        }
+    }
+        
+    class GameArtist implements drawable {
+        @Override
+        public void draw() {
+            boardArtist.draw();
+            playerArtist.draw();
+        }
+
+        public void drawWinnerLine() {
+            gc.setFill(yellowColor);
+            gc.setStroke(Color.BLACK);
+            gc.setLineWidth(20);
+            gc.setLineCap(StrokeLineCap.ROUND);
+            gc.strokeLine(winner_start_x, winner_start_y, winner_end_x, winner_end_y);
+        }
+
+        public void drawBoard() {
+            boardArtist.draw();
+        }
+
+        public void drawChip() {
+            chipArtist.draw();
+        }
+    }
+}
